08991b88370900bc54c1bc374470b66af52c274b
Mary Dear <mariya-lyuba@yandex.ru>
2013-07-03 14:34:50 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyResume::Application.config.secret_token = '1aad5f9492ded838e996728ef06c02616bb4f1b5e8b0973b0433da9871c5347308f02bf9355d4e3b478db03479b87051688868a0e61c3fd8efa47810acf3151c'
